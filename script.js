24b8c9a444e5d391ea796d831284736163031830
Jovanny <jovannyalca@gmail.com>
2023-04-11 21:36:17 -0500

    se arregla la funcionalidad de buscar dando click en el boton y tambien se agrego castillos pizza

--- script.js
+++ script.js
@@ -1,17 +1,86 @@
 
 const searchInput = document.querySelector('#searchInput');
 const cards = document.querySelectorAll('.card');
+const botonBuscar = document.querySelector('#botonBuscar');
 
-searchInput.addEventListener('input', () => {
-    const searchValue = searchInput.value.toLowerCase();
 
-    cards.forEach(card => {
-        const menu = card.getAttribute('data-menu').toLowerCase();
-
-        if (menu.includes(searchValue)) {
-            card.style.display = 'block';
-        } else {
-            card.style.display = 'none';
-        }
+    botonBuscar.addEventListener('click', () => {
+        const searchValue = searchInput.value.toLowerCase();
+    
+        cards.forEach(card => {
+            const menu = card.getAttribute('data-menu').toLowerCase();
+    
+            if (menu.includes(searchValue)) {
+                card.style.display = 'block';
+            } else {
+                card.style.display = 'none';
+            }
+        });
     });
-});
+
+
+
+
+
+/**
+ export function createTaskPlanner() {
+  let tasks = [];
+
+  return {
+    addTask(task) {
+      task.completed = false;
+      tasks.push(task);
+    },
+    removeTasks(value) {
+      if (typeof value === "number") {
+        tasks = tasks.filter((task) => task.id !== value);
+      } else {
+        tasks = tasks.filter((task) => task.name !== value);
+      }
+    },
+    getTasks() {
+      return tasks;
+    },
+    getPendingTasks() {
+      return tasks.filter((task) =>
+        !task.completed);
+    },
+    getCompletedTasks() {
+      return tasks.filter((task) =>
+        task.completed);
+    },
+
+    markTaskAsCompleted(value) {
+      let index;
+      if (typeof value === "number") {
+        index = tasks.findIndex((task)
+          => task.id === value)
+      } else {
+        index = tasks.findIndex((task)
+          => task.name === value);
+      }
+      tasks[index].completed = true;
+    },
+
+    getSortedTasksByPriority() {
+      const sortedTasks =
+        [...tasks].sort((a, b) => a.priority - b.priority);
+      return sortedTasks;
+    },
+
+    filterTasksByTag(tag) {
+      return tasks.filter((task) =>
+        task.tags.includes(tag));
+    },
+    updateTask(taskId, updates) {
+      const index =
+        tasks.findIndex((task) => task.id ===
+          taskId);
+      tasks[index] = {
+        ...tasks[index], ...updates
+      };
+    },
+  };
+}
+
+ */
