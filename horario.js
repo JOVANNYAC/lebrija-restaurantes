062a2cf90afb053fb6a2b96f79513e64083b1bae
Jovanny <jovannyalca@gmail.com>
2023-09-07 17:46:51 -0500

    se elimina codigo de la funcionalidad de mostrar el texto de horario sobre la card

--- horario.js
+++ horario.js
@@ -1,18 +1,1 @@
-document.addEventListener("DOMContentLoaded", function() {
-    const textoEstadoRestaurante = document.getElementById("estado-restaurante");
-    const horaActual = new Date().getHours();
-    const horaApertura = 8; // Hora de apertura (por ejemplo, 10:00 AM)
-    const horaCierre = 22; // Hora de cierre (por ejemplo, 8:00 PM)
 
-    if (horaActual >= horaApertura && horaActual < horaCierre) {
-        textoEstadoRestaurante.textContent = "Abierto Ahora";
-        animarTexto();
-    } else {
-        textoEstadoRestaurante.textContent = "Cerrado";
-    }
-});
-
-function animarTexto() {
-    const textoAnimado = document.querySelector(".texto-sobre-img p");
-    textoAnimado.classList.add("animacion-parpadeo");
-}
